19a0dce8500146888eba201c6c8bdeaaeb00de59
Daiveek Naik <naik.d@northeastern.edu>
2022-02-08 18:34:38 -0500

    second commit

--- daos/TuitDao.ts
+++ daos/TuitDao.ts
@@ -5,18 +5,19 @@ import path from "path/posix";
 
 
 export default class TuitDao implements TuitDaoI{
-
     async findAllTuits(): Promise<Tuit[]> {
         return await TuitModel.find();
     }
+
+    async createTuit(tuit: Tuit): Promise<any>{
+        return await TuitModel.create(tuit);
+    }
+
     findTuitsByUser = async (uid: string): Promise<Tuit[]> =>
         TuitModel.find({postedBy: uid});
-   findTuitById = async (tid: string): Promise<any> =>
-    TuitModel.findById(tid).populate(path)
-        .exec();
-   async createTuit(tuit: Tuit): Promise<any>{
-       return await TuitModel.create(tuit);
-   }
+
+    findTuitById = async (tid: string): Promise<any> =>
+        TuitModel.findById(tid);
 
    updateTuit = async (tid: string): Promise<any> =>
        TuitModel.findById(tid)
