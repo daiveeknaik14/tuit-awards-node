ed5a45f34a6f62be05fc3dd4ad8f88fa1d92c6d4
Daiveek Naik <naik.d@northeastern.edu>
2022-04-02 20:31:48 -0400

    Made changes

--- server.ts
+++ server.ts
@@ -39,10 +39,12 @@ const connectionString = "mongodb+srv://daiveeknaik:daiveek@cluster0.cphi1.mongo
 mongoose.connect(connectionString);
 dotenv.config();
 const app = express();
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:3000'
-}));
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 const SECRET = 'process.env.SECRET';
 let sess = {
